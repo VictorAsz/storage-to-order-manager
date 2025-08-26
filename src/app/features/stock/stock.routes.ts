3e39a3cfaa9808dc9e84f1218e935e8482086879
Victor Assunção <victor.a.schumann@gmail.com>
2025-08-26 02:57:55 -0300

    feat: add stock page routes

--- src/app/features/stock/stock.routes.ts
+++ src/app/features/stock/stock.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from "@angular/router";
 import { StockListPage } from "./pages/stock-list.page";
+import { StockFormPage } from "./pages/stock-form.page";
+import { StockDetailPage } from "./pages/stock-detail.page";
 
 export const STOCK_ROUTES: Routes = [
   { path: '', component: StockListPage },
+  { path: 'novo', component: StockFormPage },
+  { path: ':id/editar', component: StockFormPage },
+  { path: ':id', component: StockDetailPage },
 ]
