e2dddd83c42ca2bacdf194e483b936d6392de465
Victor Assunção <victor.a.schumann@gmail.com>
2025-08-26 00:59:12 -0300

    chore: add RouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, RouterLinkActive, RouterLink imports to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { cubeOutline, receiptOutline } from 'ionicons/icons';
+
+addIcons({ cubeOutline, receiptOutline });
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, RouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, RouterLinkActive, RouterLink],
 })
 export class AppComponent {
   constructor() {}
