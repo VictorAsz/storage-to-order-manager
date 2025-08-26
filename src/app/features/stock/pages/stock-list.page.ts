d703ea080888c4e6d97096bb7096f9fe94bbb2b3
Victor Assunção <victor.a.schumann@gmail.com>
2025-08-26 00:57:35 -0300

    feat: add stock page

---
+++ src/app/features/stock/pages/stock-list.page.ts
@@ -0,0 +1,35 @@
+import { Component, signal, effect } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonButton, IonFab, IonFabButton, IonIcon, IonNote, IonBadge } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { ProductService } from '../../../core/services/product.service';
+import { Product } from '../../../core/models/product.model';
+import { addIcons } from 'ionicons';
+import { addOutline } from 'ionicons/icons';
+
+addIcons({ addOutline });
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonButton, IonFab, IonFabButton, IonIcon, IonNote, IonBadge],
+  templateUrl: './stock-list.page.html'
+})
+export class StockListPage {
+  products = signal<Product[]>([]);
+
+  constructor(private service: ProductService, private router: Router) {
+    this.load();
+  }
+
+  async load() {
+    this.products.set(await this.service.getStock());
+  }
+
+  goNew() {
+    this.router.navigate(['estoque/novo']);
+  }
+
+  open(p: Product) {
+    this.router.navigate(['estoque', p.id]);
+  }
+}
---
+++ src/app/features/stock/stock.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { StockListPage } from "./pages/stock-list.page";
+
+export const STOCK_ROUTES: Routes = [
+  { path: '', component: StockListPage },
+]
