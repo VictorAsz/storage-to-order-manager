3183d7fb75fafe10105c74d7cdec16cde4d8995c
Victor Assunção <victor.a.schumann@gmail.com>
2025-08-26 00:54:52 -0300

    feat: add ProductService

---
+++ src/app/core/services/product.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product.model';
+import { IRepository } from '../types/irepository';
+import { GenericStorageRepository } from '../repositories/generic-storage.repository';
+import { StorageService } from './storage.service';
+import { StorageKeys } from '../types/storage-keys.enum';
+
+@Injectable({ providedIn: 'root' })
+export class ProductService {
+  private repo: IRepository<Product>;
+
+  constructor(storage: StorageService) {
+    this.repo = new GenericStorageRepository<Product>(storage, StorageKeys.PRODUCTS);
+  }
+
+  getStock() { return this.repo.getAll(); }
+  getById(id: string) { return this.repo.getById(id); }
+
+  async addProduct(product: Product): Promise<void> {
+    await this.repo.add(product);
+  }
+
+  async updateProduct(product: Product): Promise<void> {
+    await this.repo.update(product);
+  }
+
+  async deleteProduct(id: string): Promise<void> {
+    await this.repo.delete(id);
+  }
+}
