4ade809f9aa88113592813a19d7974edf985329c
Victor Assunção <victor.a.schumann@gmail.com>
2025-08-28 12:03:16 -0300

    feat: enable routes to order form page and order detail page

--- src/app/features/orders/orders.route.ts
+++ src/app/features/orders/orders.route.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { OrdersListPage } from './pages/orders-list-page';
+import { OrdersListPage } from './pages/orders-list.page';
+import { OrdersFormPage } from './pages/order-form.page';
+import { OrdersDetailPage } from './pages/order-detail-page';
 
 
 export const ORDERS_ROUTES: Routes = [
   { path: '', component: OrdersListPage },
-//{ path: 'novo', component: OrdersFormPage },
-//{ path: ':id', component: OrdersDetailPage },
+  { path: 'novo', component: OrdersFormPage },
+  { path: ':id', component: OrdersDetailPage },
 //{ path: ':id/editar', component: OrdersFormPage }, 
 ];
