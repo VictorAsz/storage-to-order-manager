3584ce020a17e94d16426a77a138e29f51b5b735
Victor Assunção <victor.a.schumann@gmail.com>
2025-09-01 17:12:58 -0300

    feat: add finished-order-detail page

---
+++ src/app/features/finished-orders/pages/finished-orders-detail.page.ts
@@ -0,0 +1,98 @@
+import { CommonModule } from "@angular/common";
+import { Component, inject, signal } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonButton, IonIcon, IonFab, IonFabButton, AlertController } from "@ionic/angular/standalone";
+import { Order } from "src/app/core/models/order.model";
+import { Product } from "src/app/core/models/product.model";
+import { OrderService } from "src/app/core/services/order.service";
+import { ProductService } from "src/app/core/services/product.service";
+
+@Component({
+    standalone: true,
+    imports: [CommonModule,  IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonButton, IonIcon, IonFab, IonFabButton],
+    templateUrl: './finished-orders-detail.page.html',
+    styleUrl: './finished-orders-detail.page.css'
+})
+export class FinisheOrderDetailPage{
+
+    private orderService = inject(OrderService);
+    private productService = inject(ProductService);
+    private route = inject(ActivatedRoute);
+    private router = inject(Router);
+    private alertController = inject(AlertController)
+    
+
+    order = signal<Order | null>(null);
+    productsMap = new Map<string, Product>();
+    id!: string;
+
+    async ngOnInit(){
+        this.id = this.route.snapshot.paramMap.get('id')!;
+        const o = await this.orderService.getById(this.id);
+        this.order.set(o);
+
+        const products = await this.productService.getStock();
+        products.forEach(p => this.productsMap.set(p.id, p))
+    }
+
+    productName(id: string){
+        return this.productsMap.get(id)?.name ?? '(removido)';
+    }
+
+
+    async delete(){
+        await this.orderService.deleteWithoutReversal(this.id!);
+    }
+
+    async reversalOrder(){
+        await this.orderService.deleteOrder(this.id!);
+    }
+
+    async deleteHandler(){
+        const alert = await this.alertController.create({
+        header: 'Confirmar exclusão',
+        message: 'Você tem certeza que deseja deletar permanentemente este pedido?',
+        buttons: [
+        {
+            text: 'Cancelar',
+            role: 'cancel',
+        },
+        {
+            text: 'Deletar',
+            handler: async () => {
+            if (this.id) {
+                await this.delete();
+                this.router.navigate(['tabs/finalizados']);
+            }
+            }
+        }
+        ]
+    });
+
+    await alert.present();
+    }
+
+    async reversalHandler(){
+        const alert = await this.alertController.create({
+        header: 'Confirmar devolução',
+        message: 'Você tem certeza que deseja devolver este pedido?',
+        buttons: [
+        {
+            text: 'Cancelar',
+            role: 'cancel',
+        },
+        {
+            text: 'Estornar',
+            handler: async () => {
+            if (this.id) {
+                await this.delete();
+                this.router.navigate(['tabs/finalizados']);
+            }
+            }
+        }
+        ]
+    });
+
+    await alert.present();
+    }
+}
