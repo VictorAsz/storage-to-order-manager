3dc71c6b88a20182c905942357292751ad543871
Victor Assunção <victor.a.schumann@gmail.com>
2025-09-01 10:19:46 -0300

    chore: rename app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-app-base',
+  appId: 'com.inventorymanager.ionic',
+  appName: 'Gerenciador de Inventário',
   webDir: 'www'
 };
 
