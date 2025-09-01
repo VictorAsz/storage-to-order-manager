1adf945211daf81230c9d5873232c93ea0980f96
Victor Assunção <victor.a.schumann@gmail.com>
2025-09-01 17:17:44 -0300

    feat: add sidebar menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/angular/standalone';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { IonApp, IonButton, IonButtons, IonContent, IonFooter, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonMenuButton, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar, MenuController } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { cubeOutline, receiptOutline } from 'ionicons/icons';
 import { App as CapacitorApp } from '@capacitor/app';
+import { Storage } from '@ionic/storage-angular';
+import { AlertController } from '@ionic/angular';
 
 addIcons({ cubeOutline, receiptOutline });
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, RouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, RouterLinkActive, RouterLink],
+  imports: [IonApp, IonRouterOutlet, RouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, RouterLinkActive, RouterLink, IonMenu, IonHeader, IonToolbar, IonContent, IonList, IonItem, IonTitle, IonButtons, IonMenuButton, IonButton, IonFooter],
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(
+    private route: Router,
+    private router: Router,
+    private menu: MenuController,
+    private storage: Storage,
+    private alertController: AlertController,
+    private menuController: MenuController
+  ) {}
    ngOnInit() {
     this.handleBackButton();
   }
@@ -26,5 +37,38 @@ export class AppComponent {
         window.history.back();
       }
     });
+  }
+
+  closeMenu() {
+    this.menuController.close(); 
+  }
+
+  async clearData(){
+    await this.storage.clear();
+  
+  }
+
+  async clearDataHandler(){
+     const alert = await this.alertController.create({
+        header: 'Confirmar exclusão',
+        message: 'Você tem certeza que deseja apagar todos os dados salvos permanentemente?',
+        buttons: [
+        {
+            text: 'Cancelar',
+            role: 'cancel',
+        },
+        {
+            text: 'Apagar',
+            handler: async () => {
+                await this.clearData();
+                this.closeMenu();
+                window.location.reload();
+                this.router.navigate(['tabs/estoque']);    
+            }
+        }
+        ]
+    });
+
+    await alert.present();
   }
 }
